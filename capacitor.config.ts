9baca790e2f22b6ef0abdcfde044ab5a3af87152
silven <elvenff@gmail.com>
2023-03-07 09:34:01 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.lek5_2023',
+  appName: 'lek5_2023',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
